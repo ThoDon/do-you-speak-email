103fcef50c5bd26df974316976043fc6bc759276
Thomas Doneux <thomas.doneux@actito.com>
2020-04-01 09:14:01 +0200

    Setup logo

--- config.js
+++ config.js
@@ -6,10 +6,9 @@ const config = {
     trailingSlash: false,
   },
   header: {
-    logo: 'https://graphql-engine-cdn.hasura.io/learn-hasura/assets/homepage/brand.svg',
-    logoLink: 'http://localhost:8000/',
-    title:
-      "<a href='http://localhost:8000/'><img class='img-responsive' src='https://graphql-engine-cdn.hasura.io/learn-hasura/assets/homepage/learn-logo.svg' alt='Learn logo' /></a>",
+    logo: 'src/logo.svg',
+    logoLink: 'https://dreamy-stonebraker-149823.netlify.com/',
+    title: '',
     githubUrl: '',
     helpUrl: '',
     tweetText: '',
